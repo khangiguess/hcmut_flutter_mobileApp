f408ac193ce3bbc8ec5e8636f7810a717308e339
khangiguess <khangtrinh007@gmail.com>
2026-07-03 11:21:47 +0700

    added wait_screen

--- hoazen/lib/main.dart
+++ hoazen/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:hoazen/views/onboarding/wait_screen.dart';
 
 void main() async {
   // Required to ensure the Flutter engine is ready before Firebase starts
@@ -20,8 +21,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hoa Zen',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
@@ -37,9 +40,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const WaitScreen(),
     );
   }
 }
