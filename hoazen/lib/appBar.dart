14ca3c632b6a9d449073db6cb4108326a13e2c16
khangiguess <khangtrinh007@gmail.com>
2026-07-20 10:47:00 +0700

    AppBart adjust

--- hoazen/lib/appBar.dart
+++ hoazen/lib/appBar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 import 'package:hoazen/sign_in_up/sign_up.dart';
 import 'package:hoazen/sign_in_up/sign_in.dart';
